68b06a9d368bb73783c78223d600a203997d88cf
gabrielgil245 <gabrielgil245@gmail.com>
2021-12-09 21:11:23 -0600

    count showing number of items added to cart

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, DoCheck {
 
+  _productCount: number = 0;
+  
   isLoggedIn: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
+    this._productCount = this.cartService.products.length;
+  }
+
+  ngDoCheck(): void {
+    this._productCount = this.cartService.products.length;
   }
 
   logout() {
