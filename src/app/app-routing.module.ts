a47f8b86a5ec288a7ead63430d7757ae04855c44
ewileyrev <ethan.wiley@revature.net>
2021-12-10 11:40:50 -0500

    adding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './page/product-page/product-page.component';
 import { SignUpComponent } from './page/sign-up/sign-up.component';
 
 const routes: Routes = [
-  {path: 'sign-up', component: SignUpComponent}
+  {path:'', redirectTo: "product", pathMatch:"full"},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'product', component: ProductPageComponent}
 ];
 
 @NgModule({
