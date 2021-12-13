7e69d2705b5c57b9c8bdce734a2cefc6c4d29909
Chris Amihan <chris.amihan@revature.net>
2021-12-12 23:32:14 -0500

    night-mode with scss updates but needs content update

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,21 +1,40 @@
-import { Component, DoCheck, OnInit } from '@angular/core';
+import { Component, HostBinding, DoCheck, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { OverlayContainer } from '@angular/cdk/overlay';
+
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit, DoCheck {
+
+  title = 'Angular material dark mode';
+
+  @HostBinding('class') className = '';
+
+  toggleControl = new FormControl(false);
 
   _productCount: number = 0;
   
   isLoggedIn: boolean = false;
 
-  constructor(private cartService: CartService, private router: Router) { }
+  constructor(private cartService: CartService, private router: Router, private dialog:MatDialog, private overlay: OverlayContainer) { }
 
   ngOnInit(): void {
+    this.toggleControl.valueChanges.subscribe((darkMode) => {
+      const darkClassName = 'darkMode';
+      this.className = darkMode ? darkClassName : '';
+      if (darkMode) {
+        this.overlay.getContainerElement().classList.add(darkClassName);
+      } else {
+        this.overlay.getContainerElement().classList.remove(darkClassName);
+      }
+    });
     this._productCount = this.cartService.products.length;
   }
 
