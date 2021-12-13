36be0ec9e1621dca402a4c179652e179fb68b32f
gabrielgil245 <gabrielgil245@gmail.com>
2021-12-13 00:51:15 -0600

    added requests for creating user and profile

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UtilService } from './util.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private utilService: UtilService, private router: Router) { }
+
+  createUser(user: any): Observable<any> {
+    return this.httpClient.post<any>(`${this.utilService.getServerDomain()}/revatrade/api/users`, user);
+  }
+
+  createUserProfile(userProfile: any, username: string): Observable<any> {
+    return this.httpClient.post<any>(`${this.utilService.getServerDomain()}/revatrade/api/users/${username}/profile/create`, userProfile);
+  }
+
+  logout() {
+    sessionStorage.clear();
+    this.router.navigateByUrl('');
+  }
+  
+  setHeaders(): void {
+    this.utilService.setHeaders();
+  }
 }
