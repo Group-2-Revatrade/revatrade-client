be9a5156d47e8a941552b28fb1aaaa4412d68878
Ellis Delgado <ellisdelgado13@gmail.com>
2021-12-14 09:00:58 -0800

    Adding orderDetail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { SignUpComponent } from './page/sign-up/sign-up.component';
 import { LoginComponent } from "./page/login/login.component";
 import { CartComponent } from './page/cart/cart.component';
 import { CheckoutComponent } from './page/checkout/checkout.component';
+import { OrderDetailsComponent } from './page/order-details/order-details.component';
 
 const routes: Routes = [
-  {path: 'product', component: ProductPageComponent},
-  {path:'', redirectTo: "product", pathMatch:"full"},
-  {path: 'sign-up', component: SignUpComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'cart', component: CartComponent},
-  {path: 'checkout', component: CheckoutComponent}
+  { path: 'product', component: ProductPageComponent },
+  { path: '', redirectTo: "product", pathMatch: "full" },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'order-details', component: OrderDetailsComponent }
 ];
 
 @NgModule({
