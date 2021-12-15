a4b77136d46a9d2697296564f9f731eed1f7f889
Leonard N <nganga25@gmail.com>
2021-12-15 07:17:19 -0800

    initial commit BE-FE communication

---
+++ src/app/models/Orders.ts
@@ -0,0 +1,20 @@
+export class Orders{
+  constructor(public orderDetailsId:number, public userId:number, public orderPrice:number, public orderQuantity:number,
+              public discount:boolean, public productId:number, public productName:String, public discountRate:number,
+              public description:string, public productPic:string, public orderTotal:number){
+
+    this.orderDetailsId = orderDetailsId;
+    this.userId = userId;
+    this.orderPrice=orderPrice;
+    this.orderQuantity=orderQuantity;
+    this.productId=productId;
+    this.productName=productName;
+    this.discount=discount;
+    this.discountRate=discountRate;
+    this.description=description;
+    this.productPic=productPic;
+    this.orderTotal =orderTotal;
+  }
+
+
+}
--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/Product';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import {UtilService} from "./util.service";
+import {Orders} from "../models/Orders";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CartService {
 
-  cart: Array<Product> = [];
+ cart: Array<Product> = [];
+ orders: Array<Orders> = [];
 
-  constructor() { }
+  constructor(private httpClient:HttpClient, private utilService: UtilService) { }
+
+  getMyOrders(userId:number){
+    console.log("Data Retrieval in progress.....");
+    return this.httpClient.get<Orders[]>(`${this.utilService.getServerDomain()}/revatrade/cart/search?userId=${userId}`)
+  }
+
+  addProductToCart(productId: number | undefined, productName: String | undefined, orderPrice: number | undefined, orderQuantity: number | undefined, userId: number | undefined){
+    return this.httpClient.get<Product[]>(`${this.utilService.getServerDomain()}/revatrade/cart/new?productId${productId},productName=${productName},orderPrice=${orderPrice},orderQuantity=${orderQuantity},userId=${userId}`)
+  }
+
+
 }
