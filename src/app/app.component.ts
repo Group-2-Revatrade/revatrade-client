c41879685bcd19975c97826165bdd19399df9e79
gabrielgil245 <gabrielgil245@gmail.com>
2021-12-16 00:08:40 -0600

    cart items persist even after refreshing the page. Will clear out by deleting items or after an order is placed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, DoCheck, HostBinding, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { OverlayContainer } from '@angular/cdk/overlay';
 
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/service/cart.service';
+import { LogoutService } from './service/logoutService/logout.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck {
  
   title = 'Angular material dark mode';
 
@@ -23,12 +24,10 @@ export class AppComponent implements OnInit {
   
   isLoggedIn: boolean = false;
 
-  userId: any = 0;
-
-  constructor(private cartService: CartService, private router: Router, private dialog:MatDialog, private overlay: OverlayContainer) { }
+  constructor(private cartService: CartService, private logoutService: LogoutService, private router: Router, private dialog:MatDialog, private overlay: OverlayContainer) { }
 
   ngOnInit(): void {
-    this.checkingCredentials();
+    this.checkCredentials();
 
     this.toggleControl.valueChanges.subscribe((darkMode) => {
       const darkClassName = 'darkMode';
@@ -39,28 +38,34 @@ export class AppComponent implements OnInit {
         this.overlay.getContainerElement().classList.remove(darkClassName);
       }
     });
+    if (sessionStorage.getItem('cart') != null) {
+      this.cartService.cart = JSON.parse(sessionStorage.cart);
+    }
     this._productCount = this.cartService.cart.length;
   }
 
   ngDoCheck(): void {
-    this._productCount = this.cartService.cart.length;
-    if(this.userId == 0 || this.userId == null) {
-      this.checkingCredentials();
+    this._productCount = this.itemsInCart();
+    if(!this.isLoggedIn) {
+      this.checkCredentials();
     }
   }
 
-  checkingCredentials() {
-    if (localStorage.getItem("userId")) {
-      this.isLoggedIn = true;
-      let temp: any = localStorage.getItem("userId");
-      this.userId = parseInt(temp);
-      console.log(this.userId);
-    }
+  itemsInCart(): number {
+    let count: number = 0;
+    this.cartService.cart.forEach(cartItem => {
+      count += cartItem.amount;
+    });
+    return count;
+  }
+
+  checkCredentials() {
+    localStorage.getItem("Revatrade-LocalStorageLocation") != null ? this.isLoggedIn = true : this.isLoggedIn = false;
   }
 
   logout() {
-    localStorage.clear();
+    this.logoutService.logout();
     this.isLoggedIn = false;
+    alert("User successfully logged out!");
   }
-
 }
