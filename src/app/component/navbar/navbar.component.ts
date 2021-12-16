e78907b32492d54aae9c8fb830b690e3b5c422e1
gabrielgil245 <gabrielgil245@gmail.com>
2021-12-16 00:02:47 -0600

    navbar component depreciated for this iteration

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -6,7 +6,6 @@ import { OverlayContainer } from '@angular/cdk/overlay';
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/service/cart.service';
 import { LogoutService } from 'src/app/service/logoutService/logout.service';
-import { LoginService } from 'src/app/service/loginService/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -15,6 +14,8 @@ import { LoginService } from 'src/app/service/loginService/login.service';
 })
 export class NavbarComponent implements OnInit, DoCheck {
 
+  // Depreciated, dark mode successfully working with app.component
+
   title = 'Angular material dark mode';
 
   @HostBinding('class') className = '';
@@ -22,11 +23,14 @@ export class NavbarComponent implements OnInit, DoCheck {
   toggleControl = new FormControl(false);
 
   _productCount: number = 0;
+
   isLoggedIn: boolean = false;
 
-  constructor(private cartService: CartService, private router: Router, private dialog:MatDialog, private overlay: OverlayContainer, private logoutService:LogoutService, private loginService:LoginService) { }
+  constructor(private cartService: CartService, private logoutService: LogoutService, private router: Router, private dialog: MatDialog, private overlay: OverlayContainer) { }
 
   ngOnInit(): void {
+    this.checkCredentials();
+
     this.toggleControl.valueChanges.subscribe((darkMode) => {
       const darkClassName = 'darkMode';
       this.className = darkMode ? darkClassName : '';
@@ -36,18 +40,14 @@ export class NavbarComponent implements OnInit, DoCheck {
         this.overlay.getContainerElement().classList.remove(darkClassName);
       }
     });
-    
     this._productCount = this.cartService.cart.length;
-    localStorage.getItem("Revatrade-LocalStorageLocation") != null ? this.isLoggedIn = true : this.isLoggedIn = false;
-    if(sessionStorage.getItem('cart') != null) {
-      this.cartService.cart = JSON.parse(sessionStorage.cart);
-    }
-    this._productCount = this.itemsInCart();
   }
 
   ngDoCheck(): void {
     this._productCount = this.itemsInCart();
-    localStorage.getItem("Revatrade-LocalStorageLocation") != null ? this.isLoggedIn = true : this.isLoggedIn = false;
+    if (!this.isLoggedIn) {
+      this.checkCredentials();
+    }
   }
 
   itemsInCart(): number {
@@ -58,10 +58,13 @@ export class NavbarComponent implements OnInit, DoCheck {
     return count;
   }
 
-  logout(event:Event){
-    event.preventDefault();
-    this.logoutService.logout();
-    alert("User successfully logged out!");
+  checkCredentials() {
+    localStorage.getItem("Revatrade-LocalStorageLocation") != null ? this.isLoggedIn = true : this.isLoggedIn = false;
   }
 
+  logout() {
+    this.logoutService.logout();
+    this.isLoggedIn = false;
+    alert("User successfully logged out!");
+  }
 }
